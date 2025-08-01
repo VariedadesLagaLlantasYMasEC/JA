80c70854c6711555bce779e4c7027caf29c6a545
Variedades Laga Llantas Y Más. <123280285+VariedadesLagaLlantasYMasEC@users.noreply.github.com>
2025-08-01 02:36:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
-      // Al cargar la página, ocultamos las cortinas
-      $('.left-curtain').css('width', '0%');
-      $('.right-curtain').css('width', '0%');
-    
-      $('.valentines-day').click(function(){
-        // Animación de desvanecimiento de los elementos del sobre
-        $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
-        $('.envelope').fadeOut(800, function() {
-          // Ocultar elementos dentro de .valentines-day
-          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
-          
-    
-          // Hacer visible la carta con una animación ondulante
-          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
-          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
-            var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
-            $(this).css('transform', 'scale(' + scale + ')');
-          }}); // Animación de ondulación
-        });
+  // Al cargar la pagina, ocultamos las cortinas
+  $('.left-curtain').css('width', '0%');
+  $('.right-curtain').css('width', '0%');
+
+  $('.valentines-day').click(function(){
+    // 🎵 Reproducir musica al hacer clic
+    document.getElementById("bg-music").play();
+
+    // Animacion de desvanecimiento de los elementos del sobre
+    $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
+    $('.envelope').fadeOut(800, function() {
+      // Ocultar elementos dentro de .valentines-day
+      $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+
+      // Hacer visible la carta con una animacion ondulante
+      $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+      $('#card').animate({'opacity': 1}, {
+        duration: 1000,
+        step: function(now, fx) {
+          var scale = 1 + Math.sin(now * Math.PI) * 0.1;
+          $(this).css('transform', 'scale(' + scale + ')');
+        }
       });
     });
+  });
+});
